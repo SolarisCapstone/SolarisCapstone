bc724266d27b26bdf5deefd5a5d5dcb86abb7905
Jonrevoir <108156604+Jonrevoir@users.noreply.github.com>
2025-03-31 22:11:40 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'batman01R',
+    user: 'username',
+    password: 'password',
     database: 'SolarisDatabase'
 });
 
@@ -25,6 +25,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/courses', (req, res) => {
     const query = "SELECT course_name, description, type FROM Courses";
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data from the database: ' + err.stack);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.json(results);
+    });
+});
+
+app.get('/api/prerequisites', (req, res) => {
+    const query = "SELECT course_name, prerequisite_name FROM Prerequisites";
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data from the database: ' + err.stack);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.json(results);
+    });
+});
+
+app.get('/api/catalogs', (req, res) => {
+    const query = "SELECT catalog_id, catalog_name, description FROM Catalogs";
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error fetching data from the database: ' + err.stack);
+            return res.status(500).send('Internal Server Error');
+        }
+        res.json(results);
+    });
+});
+
+app.get('/api/catalogcourses', (req, res) => {
+    const query = "SELECT catalog_id, course_name FROM CatalogCourses";
     db.query(query, (err, results) => {
         if (err) {
             console.error('Error fetching data from the database: ' + err.stack);
